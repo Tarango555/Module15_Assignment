e27104db9fea68fcc4e2a42d717694aca1e01068
Tarango555 <tarango.aiub@gmail.com>
2024-08-26 03:21:41 +0600

    Home page

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const http= require('http');
 
 const server= http.createServer(function (req, res){
 
+    //Home Page
+    if(req.url=='/'){
+        res.end("This is the Home Page");
+    }
+
 });
 
 server.listen(5500, function(){
