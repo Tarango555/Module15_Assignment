e8533c615f3cb5fb436300951c3d49bab1dcfcb9
Tarango555 <tarango.aiub@gmail.com>
2024-08-26 03:27:18 +0600

    About page

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const server= http.createServer(function (req, res){
         res.end("This is the Home Page");
     }
 
+    //About Page
+    if(req.url=='/about'){
+        res.end("This is the About Page");
+    }
+
+    
+
 });
 
 server.listen(5500, function(){
